2e50885b27b07afc7d6ee99616e5416e9e5c953b
AmrutaPotphode <amrutapotphode001@gmail.com>
2025-04-15 12:33:50 +0530

    added

--- script.js
+++
@@ -1,7 +0,0 @@
-// let heroImg = document.getElementById("hero-section");
-
-function changeImg(){
-    // document.getElementById("hero-section").src="hero_image1.jpg";
-    // heroImg.setAttribute= "src","('C:\Users\Akshay\Desktop\Amazon Clone\hero_image1.jpg')";
-    document.getElementById("hero-section").setAttribute("src", "C:\Users\Akshay\Desktop\Amazon Clone\hero_image1.jpg");
-}
